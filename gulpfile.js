ece38cba9c94799a1adc3bf4bdc8f40bd0f02aca
Anatolij Ostroumov <ostroumov095@gmail.com>
2016-09-30 00:46:57 +0300

    build script for lab2 machine

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var
+  deployPath = '/var/www/html/mcrms';
+
+var
+  path = require('path'),
   gulp = require('gulp'),
   debug = require('gulp-debug'),
+  inject = require('gulp-inject'),
+  replace = require('gulp-inject-string').replace,
   less = require('gulp-less'),
   bower = require('gulp-bower'),
   gulpFilter = require('gulp-filter'),
@@ -17,7 +23,12 @@ var
   fontFilter = gulpFilter(['*.eot', '*.woff', '*.svg', '*.ttf']);
 
 gulp.task('clean', function (cb) {
-  return del(['assets/*.min.js', 'assets/*.min.css','assets/*.html'], cb);
+  return del([
+    'assets/*.min.js',
+    'assets/*.min.css',
+    'assets/*.html',
+    'assets/templates/*.html'
+  ], cb);
 });
 
 gulp.task('bower', function () {
@@ -55,22 +66,74 @@ gulp.task('css', function () {
     .pipe(gulp.dest('assets'));
 });
 
-gulp.task('copy', function () {
+gulp.task('templates', function () {
   return gulp.src([
-    'index.html',
+    'templates/*.html'
+  ])
+    .pipe(gulp.dest('assets/templates'));
+
+});
+
+gulp.task('copy', ['templates'], function () {
+  return gulp.src([
     'favicon.ico',
     'logo.png'
   ])
     .pipe(gulp.dest('assets'));
 });
 
-gulp.task('default', ['clean', 'bower'], function () {
-  return gulp.start('js', 'css', 'copy');
+gulp.task('index', ['copy'], function () {
+  var
+    sources = gulp.src([
+        'assets/*.min.js',
+        'assets/*.min.css'
+      ],
+      {read: false},
+      {relative: true}
+    );
+
+  return gulp.src(['index.html'])
+    .pipe(inject(sources))
+    .pipe(replace('="/assets/', '="/'))
+    .pipe(gulp.dest('assets/'));
 });
 
-gulp.task('deploy_lab_2', ['default'], function () {
-  return gulp.src([
-    'assets/*'
-  ])
-    .pipe(gulp.dest('/var/www/html/mcrms'));
+gulp.task('default', ['clean', 'bower'], function () {
+  return gulp.start('js', 'css').start('index');
 });
+
+
+gulp.task('clean_deploy_lab_2', function (cb) {
+  return del([
+    path.join(deployPath, '*.png'),
+    path.join(deployPath, '*.jpg'),
+    path.join(deployPath, '*.ico'),
+    path.join(deployPath, '*.min.js'),
+    path.join(deployPath, '*.min.css'),
+    path.join(deployPath, '*.html'),
+    path.join(deployPath, '*.min.js'),
+    path.join(deployPath, 'templates', '*.min.js')
+  ],{force:true}, cb);
+});
+
+
+gulp.task('deploy_lab_2_templates', ['clean_deploy_lab_2'], function () {
+  return gulp.src([
+    'assets/templates/*.html'
+  ])
+    .pipe(gulp.dest(path.join(deployPath, 'templates')));
+});
+
+
+gulp.task('deploy_lab_2', ['deploy_lab_2_templates'], function () {
+    return gulp.src([
+      'assets/favicon.ico',
+      'assets/*.png',
+      'assets/*.jpg',
+      'assets/*.min.js',
+      'assets/*.min.css',
+      'assets/*.html'
+    ])
+      .pipe(gulp.dest(deployPath))
+  }
+);
